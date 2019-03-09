0eac8cd11b005522029dd2edd8172737c5a6f27a
erikb <erikburton1@gmail.com>
2019-03-09 11:50:16 -0700

    Generates Articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  # Resource used for a collection of similar objects with CRUD operations
   resources :articles
 
   root 'welcome#index'
-  
+
 end
