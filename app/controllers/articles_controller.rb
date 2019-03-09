fcc1ba5b97e41ef62cde1a880c35822ab1547588
erikb <erikburton1@gmail.com>
2019-03-09 13:54:03 -0700

    Change create action to use Article model.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # remove this later on
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article 
   end
 end
 
