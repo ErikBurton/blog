c6d2295ca48fd8f289f67a886f7d8fd6862c0f8b
erikb <erikburton1@gmail.com>
2019-03-09 11:54:13 -0700

    Adds NEW method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def new
+  end
 end
+
+# A contoller is a class that is defined to inherit from ApplicationController. It's inside this class that defined methods will become the actions for this controller. These actions perfrom CRUD operations on the articles within the app.
