e912682d1d22da15330c686ad2eb8d3ff420f391
erikb <erikburton1@gmail.com>
2019-03-09 14:01:59 -0700

    Adds SHOW action in contoller and adds SHOW view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
-    redirect_to @article 
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
+
 
 # A contoller is a class that is defined to inherit from ApplicationController. It's inside this class that defined methods will become the actions for this controller. These actions perfrom CRUD operations on the articles within the app.
