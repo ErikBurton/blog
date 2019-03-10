6a702403d874f86a8968fd03765c1d9d0c0aa5b6
erikb <erikburton1@gmail.com>
2019-03-10 14:48:23 -0600

    Adds comments controller and fills in views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   # Resource used for a collection of similar objects with CRUD operations
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 
